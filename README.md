aee8cdd1347be9d4b0fa6949fcecdd1bfd8c10f4
Raleigh Littles <raleighlittles@gmail.com>
2024-06-27 11:03:15 -0700

    created credential generator script, updated README

--- README.md
+++ README.md
@@ -10,7 +10,18 @@ By filling out a simple CSV file and creating a credentials file, you can have a
 
 In addition to the formatting, this script can automatically retrieve user and group IDs from the username provided, which is useful for systems with multiple users.
 
-# Usage
+# Setup/Usage
+
+Create the top-level directory for your shares folder; this will usually require root. Change the ownership of that directory to your current user.
+
+e.g.
+
+```bash
+sudo mkdir <MY-SHARE-FOLDER>
+sudo chown -R $USER:$USER <MY-SHARE-FOLDER>
+```
+
+Then run the script:
 
 ```
 usage: fstab_generator_for_smb.py [-h] -f INPUT_CSV_FILE -i REMOTE_IP_ADDRESS [-r REMAINING_TEXT]
@@ -24,3 +35,16 @@ options:
   -r REMAINING_TEXT, --remaining-text REMAINING_TEXT
                         Any remaining text to add in the line
 ```
+
+It'll produce the output that you need to then copy into your `/etc/fstab` file. Once the fstab file has been saved, open a separate terminal window with dmesg, and in the current one, do:
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl restart remote-fs.target
+```
+
+You should then see a bunch of messages in the dmesg window about your mounts.
+
+# Personal
+
+For my own servers that use QNAP, you need to add: `nounix 0 0` to the end of each line.
---
+++ credential_generator.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+
+
+def generate_credential_file(username, password, domain):
+
+    credential_file_path = os.path.join( os.getenv("HOME"), ".smbcredentials")
+
+    if os.path.exists(credential_file_path):
+        raise FileExistsError(f"[ERROR] The file {credential_file_path} already exists")
+
+    with open(credential_file_path, "w") as credential_file:
+        credential_file.write(f"username={username}\n")
+        credential_file.write(f"password={password}\n")
+
+        if domain:
+            credential_file.write(f"domain={domain}\n")
+    
+
+if __name__ == "__main__":
+    argparse_parser = argparse.ArgumentParser()
+
+    argparse_parser.add_argument("-u",
+                                 "--username",
+                                 type=str,
+                                 required=True,
+                                 help="The username to use")
+    argparse_parser.add_argument("-p",
+                                 "--password",
+                                 type=str,
+                                 required=True,
+                                 help="The password to use")
+    argparse_parser.add_argument("-d",
+                                 "--domain",
+                                 type=str,
+                                 required=False,
+                                 help="The domain to use")
+
+    argparse_args = argparse_parser.parse_args()
+
+    generate_credential_file(argparse_args.username, argparse_args.password,
+                             argparse_args.domain)
